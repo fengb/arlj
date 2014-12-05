137dd2bab9409a53697c23aec6ce0e80829188e9
fengb <contact@fengb.info>
2014-12-04 18:19:30 -0600

    Refactored call stack to expose raw Arel

--- lib/arlj.rb
+++ lib/arlj.rb
@@ -6,6 +6,8 @@ module Arlj
 
   alias_method :left_joins, :arlj
   alias_method :left_joins_aggregate, :arlj_aggregate
+  alias_method :left_joins_arel, :arlj_arel
+  alias_method :left_joins_aggregate_arel, :arlj_aggregate_arel
 
   def self.memoize!
     Arlj::Base.memoize!
--- lib/arlj/base.rb
+++ lib/arlj/base.rb
@@ -3,7 +3,7 @@ module Arlj
     def self.memoize!
       require 'memoist'
       self.extend Memoist
-      self.memoize :arlj_aggregate_sources
+      self.memoize :arlj_sql, :arlj_aggregate_sql
     end
 
     def arlj(assoc)
@@ -11,9 +11,7 @@ module Arlj
       #  LEFT JOIN [assoc]
       #         ON [assoc].source_id = source.id
 
-      refl = reflect_on_association(assoc)
-      sources = arlj_arel_sources(refl.klass.arel_table, refl.foreign_key)
-      joins(sources)
+      joins(arlj_sql(assoc))
     end
 
     # Example usage:
@@ -25,8 +23,45 @@ module Arlj
       #             GROUP BY [assoc].source_id) arlj_aggregate_[assoc]
       #         ON [assoc].source_id = source.id
 
-      sources = arlj_aggregate_sources(assoc, *args)
-      joins(sources)
+      joins(arlj_aggregate_sql(assoc, *args))
+    end
+
+    def arlj_arel(assoc)
+      refl = reflect_on_association(assoc)
+      arlj_left_join_arel(refl.klass.arel_table, refl.foreign_key)
+    end
+
+    def arlj_sql(assoc)
+      arlj_arel(assoc).join_sources
+    end
+
+    def arlj_aggregate_arel(assoc, *args)
+      options = args.extract_options!
+
+      refl = reflect_on_association(assoc)
+      refl_arel = refl.klass.arel_table
+
+      join_name = "arlj_aggregate_#{refl.table_name}"
+
+      columns = [refl_arel[refl.foreign_key]]
+      args.each do |thunk|
+        columns << parse_thunk(refl, assoc, refl_arel, thunk)
+      end
+      options.each do |thunk, name|
+        columns << parse_thunk(refl, assoc, refl_arel, thunk, name)
+      end
+
+      subq_arel =
+        refl_arel.project(columns).
+                  from(refl_arel).
+                  group(refl_arel[refl.foreign_key]).
+                  as(join_name)
+
+      arlj_left_join_arel(subq_arel, refl.foreign_key)
+    end
+
+    def arlj_aggregate_sql(assoc, *args)
+      arlj_aggregate_arel(assoc, *args).join_sources
     end
 
     private
@@ -63,36 +98,9 @@ module Arlj
       arel[column].send(func).as(name)
     end
 
-    def arlj_aggregate_sources(assoc, *args)
-      options = args.extract_options!
-
-      refl = reflect_on_association(assoc)
-      refl_arel = refl.klass.arel_table
-
-      join_name = "arlj_aggregate_#{refl.table_name}"
-
-      columns = [refl_arel[refl.foreign_key]]
-      args.each do |thunk|
-        columns << parse_thunk(refl, assoc, refl_arel, thunk)
-      end
-      options.each do |thunk, name|
-        columns << parse_thunk(refl, assoc, refl_arel, thunk, name)
-      end
-
-      subq_arel =
-        refl_arel.project(columns).
-                  from(refl_arel).
-                  group(refl_arel[refl.foreign_key]).
-                  as(join_name)
-
-      arlj_arel_sources(subq_arel, refl.foreign_key)
-    end
-
-    def arlj_arel_sources(arel, foreign_key)
-      arel_join =
-        arel_table.join(arel, Arel::Nodes::OuterJoin).
-                     on(arel[foreign_key].eq(arel_table[self.primary_key]))
-      arel_join.join_sources
+    def arlj_left_join_arel(arel, foreign_key)
+      arel_table.join(arel, Arel::Nodes::OuterJoin).
+                   on(arel[foreign_key].eq(arel_table[self.primary_key]))
     end
   end
 end
